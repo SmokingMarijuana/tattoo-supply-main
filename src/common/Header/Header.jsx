391047062812105edbaed7373f865b96627ccfa0
SmokingMarijuana <kebabrumeno3euro@gmail.com>
2025-12-06 00:58:39 +0100

    Commit 05 12

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -5,6 +5,7 @@ import { LinkButton } from '../LinkButton/LinkButton';
 import { useSelector, useDispatch } from "react-redux";
 import { logout, selectToken } from "../../pages/userSlice";
 import { Link } from 'react-router-dom';
+import cart from '../../assets/cart (1).svg';
 
 export const Header = () => {
     const dispatch = useDispatch();
@@ -113,9 +114,10 @@ export const Header = () => {
                         path={"/register"}
                         title={"Categorie"}
                     />
-                    <Link>
-                        <img src = '../../assets/shopping-cart.png' className = "w-5 min-w-5" alt ="cart"/>
-                        <p className='absolute right-[-5px] bottom[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+
+                    <Link to = '/cart' className='relative'>
+                        <img src = {cart} className='carrello w-6 min-w-5' alt ="cart"/>
+                        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[13px]'>10</p>
                     </Link>
                 </>
             )}
@@ -123,10 +125,12 @@ export const Header = () => {
     );
 
     return (
-        <>
+        <>    
             <header className="header">
                 {/* Scritta centrale in alto */}
+                
                 <div className="header-bar header-button">
+                    
                     <LinkButton
                         className={"header-brand"}
                         path={"/"}
--- src/common/Hero/Hero.jsx
+++ src/common/Hero/Hero.jsx
@@ -8,6 +8,7 @@ export const Hero = () => {
         <div className="hero-text">
           <h1><b>Prodotti professionali<br></br> per tatuatori</b></h1><br></br>
           <p>Aghi, inchiostri e attrezzatura di qualità per il tuo studio.</p>
+                    
           <button action = "/shop"><a href="/shop">Scopri i prodotti</a></button>
         </div>
       </div>
